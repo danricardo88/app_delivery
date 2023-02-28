1cb196db86c0096e3918b30a375c817cc5540958
Camila Wolter <camila0wolter@gmail.com>
2023-02-28 16:37:53 -0300

    feat: criação do model, migration e seeders de products

---
+++ back-end/src/database/migrations/20230228192225-create-product.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   await queryInterface.createTable('products', { 
+    id: {
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+    },
+    price: {
+      type: Sequelize.DOUBLE,
+      allowNull: false,
+    },
+    urlImage: {
+      type: Sequelize.STRING(200),
+      field: 'url_image',
+      allowNull: false,
+    },
+    });
+  
+  },
+
+  async down (queryInterface, Sequelize) {
+   await queryInterface.dropTable('products');
+    
+  }
+};
