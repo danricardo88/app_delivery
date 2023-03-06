c3c25439105c4c64781a921debfc87460cb9d423
Renan Pinto <renanrnpinto@gmail.com>
2023-03-06 16:19:18 -0300

    Refatoranto Backend LOgin

--- back-end/src/controllers/loginController.js
+++ back-end/src/controllers/loginController.js
@@ -1,11 +1,12 @@
 const loginService = require('../services/loginService');
+const md5 = require('md5');
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  const result = await loginService.login(email, password);
-  console.log('r');
-  if (!result) {
-    return res.status(404).json('User not found');
+  const hash = md5(password);
+  const result = await loginService.login(email);
+  if (!result || hash !== result.password) {
+    return res.status(404).json({ message: 'Invalid fields' });
   }
   return res.status(200).json(result);
 };
--- back-end/src/services/loginService.js
+++ back-end/src/services/loginService.js
@@ -1,9 +1,8 @@
 const { User } = require('../database/models');
-const md5 = require('md5');
 
-const login = async (email, password) => {
-  const userLogin = await User.findOne({ where: { email } })
-    return userLogin;
+const login = async (email) => {
+  const userLogin = await User.findOne({ where: { email } }); 
+  return userLogin;
 };
 
 module.exports = { 
