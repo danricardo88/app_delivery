a7dfc3935004869881be51eb8ca62c9385954f2b
Camila Wolter <camila0wolter@gmail.com>
2023-03-09 10:01:52 -0300

    fix: correção no login

--- back-end/src/controllers/loginController.js
+++ back-end/src/controllers/loginController.js
@@ -1,20 +1,10 @@
-const md5 = require('md5');
 const loginService = require('../services/loginService');
-const tokenValidation = require('../utils/tokenValidation');
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  
-  const hash = md5(password);
-
-  const result = await loginService.login(email);
-  const { name, role, id } = result;
-  if (!result || hash !== result.password) {
-    return res.status(404).json({ message: 'Invalid fields' });
-  }
-
-  const token = await tokenValidation.createToken(email);
-  return res.status(200).json({ token, name, role, email, id });
+  const result = await loginService.login(email, password);
+  if (result.error) return res.status(404).json({ message: result.message });
+  return res.status(200).json({ ...result.message })
 };
 
 const findAllUsers = async (_req, res) => {
